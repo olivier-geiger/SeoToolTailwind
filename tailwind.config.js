42e5823d5c986b675e51f9a2a618e532918fd20f
olivier-geiger <69504966+olivier-geiger@users.noreply.github.com>
2021-06-02 17:12:54 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        extend: {
+            minHeight: {
+                500: "500px",
+                650: "650px"
+            }
+        }
+    },
+    variants: {
+        extend: {
+            borderWidth: ['hover']
+        }
+    }
+}
